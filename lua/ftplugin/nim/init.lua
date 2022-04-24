73f376bbc0edd95d52a0a32876cad25ef8632ef8
Thadeu de Paula <thadeudepaula@gmail.com>
2022-04-24 12:06:37 -0300

    Nim: restricted lsp to nom nimble files

--- lua/ftplugin/nim/init.lua
+++ lua/ftplugin/nim/init.lua
@@ -4,6 +4,7 @@ function ft.ftplugin()
     local opt = vim.opt_local
     local map = vim.api.nvim_buf_set_keymap
     local bindopt = {noremap=true,silent=true}
+    local filename = vim.api.nvim_buf_get_name(0)
     local function script_path()
         local str = debug.getinfo(2, "S").source:sub(2)
         return str:match("(.*/)")
@@ -17,28 +18,33 @@ function ft.ftplugin()
 
 
     map(0, "n", "\\x", "<cmd>!nim r --spellSuggest --showAllMismatches %<Enter>", bindopt)
+    map(0, "n", "\\c", "<cmd>!nim compile --spellSuggest --showAllMismatches %<Enter>", bindopt)
 
-    require("lsp-config").setup("nimls", {
-       -- We use a wrapper, so it not keeps hanging :)
-       cmd = { script_path().."/nimlswrapper" }
-       -- Defaults...
-       -- cmd = { "nimlsp" },
-       -- filetypes = { "nim" },
-       -- single_file_support = true,
-    })
+    -- Nimble files get broken
+    if filename:match(".nims?$") then
+       require("lsp-config").setup("nimls", {
+          -- We use a wrapper, so it not keeps hanging :)
+          cmd = { script_path().."/nimlswrapper" }
+          -- Defaults...
+          -- cmd = { "nimlsp" },
+          -- filetypes = { "nim" },
+          -- single_file_support = true,
+       })
+    end
 
 end
 
 function ft.syntax()
-    vim.cmd[[set syntax=nim]]
-    vim.cmd[[syntax match nimDelimiter "[\[\]\(\)\{\},]"]]
-    vim.cmd[[hi! link nimDelimiter Delimiter]]
+   vim.cmd[[set syntax=nim]]
+   vim.cmd[[syntax match nimDelimiter "[\[\]\(\)\{\},]"]]
+   vim.cmd[[hi! link nimDelimiter Delimiter]]
 
-    vim.cmd[[syntax match nimOperator "[=+*/<>@$~&%|!?^.:\\-]" ]]
-    vim.cmd[[hi! link nimOperator Operator]]
+   vim.cmd[[syntax match nimOperator "[=+*/<>@$~&%|!?^.:\\-]" ]]
+   vim.cmd[[hi! link nimOperator Operator]]
 
-    vim.cmd[[hi! link nimStatement Statement]]
-    vim.cmd[[hi! link nimFunction Function]]
+   vim.cmd[[hi! link nimStatement Statement]]
+   vim.cmd[[hi! link nimFunction Identifier]]
+   vim.cmd[[hi! link nimBuiltin  Type]]
 end
 
 -- nimlsp doesn't works without a filesystem file. So when opening
